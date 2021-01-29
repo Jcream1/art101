803f50c007aecfc1bcd0b2ddd702898e0b4960ba
Jcream <jzhan190@ucsc.edu>
2021-01-29 16:31:55 +0800

    lab4

---
+++ lab4/js/lab4.js
@@ -0,0 +1,12 @@
+// Set computer's choice
+	// set three values rock paper and scissor
+		// randomly choose one from it
+		// print it out
+// Accept player's input
+	// save the input value
+		// print it out
+// Compare those two values
+	// set conditions
+		//loop through each condition
+			// print the result
+// Test for win
