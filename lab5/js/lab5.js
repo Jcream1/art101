fc1a5b03d91b37ae1bfae8b6e2c221fe4820dc9c
Jcream1 <jzhan190@ucsc.edu>
2021-02-02 20:51:05 +0800

    lab5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,22 @@
+/*
+ * Author: Jiabin Zhang <jzhan190@ucsc.edu>
+ * Created: 31 January
+ */
+
+ // Define Variables
+ make = "Porsche";
+ model = "Boxster";
+ color = "Silver";
+ year = 2003;
+ var ownIt = new Boolean(0);
+
+ // calculate
+ age = 2021 - year;
+
+ // output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("ownIt: " + ownIt + "<br>");
+ document.writeln("Age: " + age + "<br>");
