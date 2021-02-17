386afbaf94ef0d84c0f00a9aca417c24e6cc32e7
jcream12 <jzhan190ucsc.edu>
2021-02-17 23:49:21 +0800

    lab10

---
+++ lab10/js/lab10.js
@@ -0,0 +1,50 @@
+/*
+ * Author: Jiabin Zhang <jzhan190@ucsc.edu>
+ * Created: 1 January
+ */
+
+// define variables
+function shuffleArray(array) {
+  var currentIndex = array.length, temValue, randomIndex;
+
+  while(0!==currentIndex){
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temValue;
+  }
+  return array;
+}
+
+function toTitleCase(str) {
+  return str.toString().replace(/(^|\s)\S/g, function(t) { return t.toUpperCase() });
+}
+
+function sortUserName(userName){
+  return userName.toLowerCase().split("").sort().join("");
+}
+
+function randomizeName(userName){
+  var nameArray = userName.toLowerCase().split("");
+  console.log("nameArray =", nameArray);
+
+  var shuffledArray = shuffleArray(nameArray);
+  console.log("shuffleArray =", shuffledArray);
+  var shuffledString = shuffledArray;
+  console.log("shuffledString =", shuffledString);
+  var newName = toTitleCase(shuffledString);
+
+  return newName;
+}
+
+var submitButton = document.getElementById('submit-button');
+submitButton.addEventListener("click", function(){
+  console.log('submitted');
+  var userName = document.getElementById('name-field').value;
+  var sortedName = sortUserName(userName);
+  var randomName = randomizeName(userName);
+  var outputArea = document.getElementById('output');
+  outputArea.innerText = randomName;
+})
